107bf9815009a5fc0aeef9af95a1b369c0f8f2d5
TMR <kingmohanreddy143@gmail.com>
2020-07-11 12:47:51 +0530

    creating list operations

---
+++ list_operations.py
@@ -0,0 +1,15 @@
+#creating list operations
+
+lst = [2,5,6,7,8,25,64]
+
+#printing lst
+print(lst)
+#using append
+lst.append(56)
+print(lst)
+#using copy
+lst.copy()
+print(lst)
+#using clear
+lst.clear()
+print(lst)
